8e4d9756bb18f1e15f5473f920cc825a3bab22f0
jchoi17 <joan.choi@ucla.edu>
2021-04-04 20:31:20 -0700

    initial push

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,56 @@
+// from data.js
+var tableData = data;
+console.log(tableData);
+
+// set reference to tbody
+var tbody = d3.select("tbody");
+
+// get values for each column
+tableData.forEach(function(UFOdata) {
+    console.log(UFOdata);
+    
+    // append one row for each sighting
+    var sighting = tbody.append("tr");
+
+    //use object.entries to separate key and value
+    Object.entries(UFOdata).forEach(function([key, value]) {
+      console.log(key, value);
+      var rowDetail = sighting.append("td");
+      rowDetail.text(value);
+    });
+  });
+
+
+// Select filter button
+var button = d3.select("#filter-btn");
+
+//create event handler
+button.on("click", function() {
+
+    tbody.html("");
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    // filter data using mapping
+    var filteredDate = tableData.filter(d => d.datetime === inputValue);
+    // console.log filtered data by date
+    console.log(filteredDate);
+
+
+    filteredDate.forEach(function(i) {
+
+    console.log(i);
+    // append row for each sighting
+    var sighting = tbody.append("tr");
+    
+    //use object.entries to separate key and value
+    Object.entries(i).forEach(function([key, value]) {
+        console.log(key, value);
+        var rowDetail = sighting.append("td");
+        rowDetail.text(value);
+    });
+});
+});
